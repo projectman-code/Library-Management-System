0d2142e244302439ef28c39ee695f7f7fa741366
adoptapets01 <adopta.pets01@gmail.com>
2026-08-16 08:32:51 +0530

    Implemented delete, edit, and return features

--- LMS/library/views.py
+++ LMS/library/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect,HttpResponse
+from datetime import date
 from .forms import StudentsForm, BookForm, Book_IssueForm,Book_instanceForm
 from .models import Students, Book, Book_Issue,BookInstance
 
@@ -42,7 +43,6 @@ def add_book_issue(request):
     if request.method=="POST":
         form = Book_IssueForm(request.POST)
         if form.is_valid():
-            # save data
             unsaved_form=form.save(commit=False)
             book_to_save=BookInstance.objects.get(id=unsaved_form.book_instance.id)
             book_to_save.Is_borrowed=True
@@ -70,7 +70,7 @@ def view_bissue(request):
 def edit_student_data(request,roll):
     try:
         if request.method == "POST":
-            std=Students.objects.get(id=request.session['id'])    
+            std=Students.objects.get(id=request.session['id'])
             form = StudentsForm((request.POST),instance=std)
             if form.is_valid():
                 form.save()
@@ -85,19 +85,54 @@ def edit_student_data(request,roll):
         print(f"{error} occured at edit_student_data view")
 
 def edit_book_data(request,id):
-    return HttpResponse(f"<label>A book with ID: {id} could not be edited...</label><h2>The feature is comming soon</h2>")
+    try:
+        if request.method == "POST":
+            bk=Book.objects.get(id=request.session['book_id'])
+            form = BookForm((request.POST),instance=bk)
+            if form.is_valid():
+                form.save()
+            del request.session['book_id']
+            return redirect("/view_books")
+        else:
+            instance=BookInstance.objects.get(id=id)
+            book_to_edit=instance.book
+            book=BookForm(instance=book_to_edit)
+            request.session["book_id"]=book_to_edit.id
+            return render(request,'edit_book_data.html',{'book':book})
+    except Exception as error:
+        print(f"{error} occured at edit_book_data view")
 
 def delete_student(request,roll):
-    return HttpResponse(f"<h2>Delete Student</h2><label>Student with Roll Number: {roll} could not be deleted...</label><h2>The feature is comming soon</h2>")
-    pass
+    student = Students.objects.get(roll_number=roll)
+    student.delete()
+    return redirect('/show_students')
 
 def delete_book(request,id):
-    return HttpResponse(f"<h2>Delete Book</h2><label>Book with ID: {id} could not be deleted..</label><h2>The feature is comming soon</h2>")
+    book = BookInstance.objects.get(id=id)
+    book.delete()
+    return redirect('/view_books')
 
-def return_issued_book(request,id):   
-    obj=Book_Issue.objects.get(id=id)
-    return HttpResponse(f"<h2>Return Issued Book</h2><label>Book <i>{obj.book_instance.book.book_title}</i> issued to <i>{obj.student.fullname}</i> could not be returned..</label><h2>The feature is comming soon</h2>")
+def return_issued_book(request,id):
+    obj = Book_Issue.objects.get(id=id)
+    obj.date_returned = date.today()
+    obj.save()
+    obj.book_instance.Is_borrowed = False
+    obj.book_instance.save()
+    return redirect('/view_books_issued')
 
 def edit_issued(request, id):
-    obj=Book_Issue.objects.get(id=id)
-    return HttpResponse(f"<h2>Edit Issued Book</h2><label>Book <i>{obj.book_instance.book.book_title}</i> issued to <i>{obj.student.fullname}</i> could not be edited..</label><h2>The feature is comming soon</h2>")
+    try:
+        if request.method == "POST":
+            issue=Book_Issue.objects.get(id=request.session['issue_id'])
+            form = Book_IssueForm((request.POST),instance=issue)
+            if form.is_valid():
+                form.save()
+            del request.session['issue_id']
+            return redirect("/view_books_issued")
+        else:
+            issue_to_edit=Book_Issue.objects.get(id=id)
+            issue=Book_IssueForm(instance=issue_to_edit)
+            request.session["issue_id"]=issue_to_edit.id
+            return render(request,'edit_issued.html',{'issue':issue})
+    except Exception as error:
+        print(f"{error} occured at edit_issued view")
